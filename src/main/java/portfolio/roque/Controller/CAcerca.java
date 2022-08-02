cc90f37adb262aa9df17dd396a541c35f33d683f
Roque Giollo <giolloroque@gmail.com>
2022-08-02 18:13:06 -0300

    combio direcciones

--- src/main/java/portfolio/roque/Controller/CAcerca.java
+++ src/main/java/portfolio/roque/Controller/CAcerca.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acerc") //parametro a cambiar dependiendo el componente
-@CrossOrigin(origins = "https://backend-portfolio3.herokuapp.com")
+@CrossOrigin(origins = "https://frontendporfolio.web.app")
 public class CAcerca {
     @Autowired
     SAcerca sAcerca;
